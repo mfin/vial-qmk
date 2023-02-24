68dce57442c1b03553afc21286e87f9dfef84582
Matjaž Finžgar <matjaz@finzgar.net>
2023-02-24 12:20:50 +0100

    feat: init vial for sweep

---
+++ keyboards/ferris/sweep/keymaps/vial-custom/config.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x47, 0x97, 0x7E, 0x32, 0xC9, 0xAC, 0x13, 0xF3}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 2, 3, 0, 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 1, 0, 2, 4 }
+
+// Good defaults for home row modifiers
+#define TAPPING_TERM 230
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
+#define SPLIT_TRANSPORT_MIRROR
